7f57cdcb7aebb12082c1c951050b5af1c94d7ccd
shaon-sarker <shaon180054+github@diit.info>
2023-08-20 21:47:33 +0600

    first commit

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps(['person', 'messageToChild', 'messageFromChild'])
+defineEmits(['update:messsageFromChild'])
+</script>
+<template>
+    <div class="p-5 border-2 border-gray-300 border-s-fuchsia-50">
+        <div class="max-w-lg mx-auto p-4 bg-white rounded-md shadow-md flex flex-col items-center justify-center mb-5">
+            <!-- <h1 class="text-xl font-bold text-gray-900">
+                {{ messageToChild }}
+            </h1> -->
+        </div>
+
+        <div class="max-w-md mx-auto p-10 bg-white rounded-md shadow-md flex flex-col items-center justify-center">
+            <img class="w-20 h-20 rounded-full ring-4 ring-sky-600" src="https://media.geeksforgeeks.org/wp-content/cdn-uploads/20220131222755/Vue.js-Tutorial.png" alt="Vue Js ">
+            <div class="flex flex-col items-center justify-center pt-5 pb-5">
+                <p class="text-lg font-semibold">{{ person.name }}</p>
+                <p class="text-sm text-gray-500">{{ person.title }}</p>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref} from 'vue'
+import ChildComponent from './ChildComponent.vue';
+const person = {
+    name:'Vue Js Course',
+    title:'Frontend Developer',
+    url:'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20220131222755/Vue.js-Tutorial.png'
+}
+
+// const messageToChild = 'Welcome to Child Component'
+const messageFromChild = ref('')
+</script>
+<template>
+    <div class="h-screen bg-gradient-to-r from-red-100 to-sky-100 flex items-center justify-center flex-col">
+        <h1 class="font-bold ">{{ messageFromChild }}</h1>
+        <ChildComponent :person = person v-model:messageFromChild="messageFromChild"></ChildComponent>
+    </div>
+</template>
